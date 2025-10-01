bb5067db8846f288abb0e63985ce2e2859aefe39
benbrittle <benbrittle@pm.me>
2025-10-01 18:28:25 +0100

    Well this was stupid

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+from astro_pi_orbit import ISS
+
+iss = ISS()
+
+point = iss.coordinates()
+coordinates = (point.latitude, point.longitude)
+print(coordinates)
+elevation = point.elevation.km
+print(elevation)
