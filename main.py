6c9086164ece47c6b05feb1422ae2c3d462f0089
benbrittle <benbrittle@pm.me>
2026-01-13 11:54:25 +0000

    balls

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from astro_pi_orbit import ISS
+import time
 
 iss = ISS()
 
 point = iss.coordinates()
-coordinates = (point.latitude, point.longitude)
+coordinates = (point.latitude.radians, point.longitude.radians)
 print(coordinates)
 elevation = point.elevation.km
 print(elevation)
+
+radius = elevation+6,371
