c31d7dfc0966acd15b54d26c71444aa64fb1f336
benbrittle <benbrittle@pm.me>
2026-01-15 09:56:04 +0000

    get points over time

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from astro_pi_orbit import ISS
+import numpy as np
 import time
 
 def getPointData():
     iss = ISS()
 
     point = iss.coordinates()
-    return point.latitude.radians, point.longitude.radians, point.elevation.km
+    return np.array([point.latitude.radians, point.longitude.radians, point.elevation.km, time.time()])
 
-print(getPointData())
+sampleNum = 5
+
+
+dataList = np.zeros(shape=(sampleNum, 4))
+for x in range(0, sampleNum):
+    dataList[x] = getPointData()
+    time.sleep(10)
+print(dataList[:,2])
